55f4d410e01b6719010b9280659bdddf073d3c66
조경진 <cho881020@gmail.com>
2022-05-21 14:48:38 +0900

    메세지 입력 화면 구성 + 다른 화면으로 이동까지.  - 문구를 들고가지는 못하는 상황.

--- app/src/main/java/com/neppplus/intent_20220521/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220521/MainActivity.kt
@@ -20,5 +20,16 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btnSend.setOnClickListener {
+
+//            1. 입력된 메세지를 변수에 저장
+            val inputMessage = edtMessage.text.toString()
+
+//            2. 메세지를 "들고" ViewMessage 화면으로 이동
+            val myIntent = Intent( this, ViewMessageActivity::class.java )
+            startActivity(myIntent)
+
+        }
+
     }
 }
