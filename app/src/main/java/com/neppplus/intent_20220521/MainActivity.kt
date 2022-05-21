f7a7fb6093b5d5a5c076ec40b9a45acef6c84823
조경진 <cho881020@gmail.com>
2022-05-21 14:34:07 +0900

    Intent로 메인에서 > Other로 이동

--- app/src/main/java/com/neppplus/intent_20220521/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20220521/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.intent_20220521
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         btnMove.setOnClickListener {
 
 //            다른 화면으로 이동하기
+
+            val myIntent = Intent( this, OtherActivity::class.java )
+            startActivity(myIntent)
 
 
         }
